bb19af787b0b928b65c3713952d85892458d3577
suvitha sivaraj <24bcs254@kgcas.com>
2025-07-13 23:11:25 +0530

    index.js
    Signed-off-by: suvitha sivaraj <24bcs254@kgcas.com>

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
-var x=3
-let y='js'
-const iscloudy=true;
-console.log(name+" "+iscloudy+''+x)
+var x=3;
+
+let name="yuvaraj";
+
+const pi=3.14;
+
+
+
+console.log(x);
+
+console.log(name);
+
+console.log(pi);
+
+document.write(x);
+
+document.write(name);
+
+document.write(pi);
+
+
+
+function add(a,b){
+
+    console.log("this is function "+(a+b));
+
+}
+
+ add(4,5);
+
+    let a=parseInt(prompt("entere the number"));
+
+    let b =parseInt(prompt("enter the second number:"));
+
+
+
+alert(a+b);
