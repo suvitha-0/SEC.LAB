13f9b19c8fba6bae1f9c9799a6d278563e8d911c
suvitha sivaraj <24bcs254@kgcas.com>
2025-07-21 15:20:08 +0530

    Add files via upload
    Signed-off-by: suvitha sivaraj <24bcs254@kgcas.com>

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
-var x=3;
-
-let name="suvitha";
-
-const pi=3.14;
-
-
-
-console.log(x);
-
-console.log(name);
-
-console.log(pi);
-
-document.write(x);
-
-document.write(name);
-
-document.write(pi);
-
-
-
-function add(a,b){
-
-    console.log("this is function "+(a+b));
-
+//variable declared outside any function or block are in the global scope.
+//They are accessible anywhere in the script.
+let x=10;
+function Sendmessage(){
+    console.log("Hai Js");
+    console.log(x);//within
 }
+Sendmessage();
+  console.log(x);//outside
 
- add(4,5);
+//function scope
+//only accessible in the functiobn in which they are declared.  
+function Sendmessage(){
+    let x=10;
+    console.log("Hai Js");
+    console.log(x);//within
+}
+Sendmessage();
 
-    let a=parseInt(prompt("entere the number"));
+//block scope
+//The block scope of the variable means that the variable
+//is accessible within the block that is between the curly braces
+function Check(){
+    if(true){
+        let name="Webdevelopment";
+        console.log("Block scope");
+        console.log(name);
+    }
+}
+Check()
 
-    let b =parseInt(prompt("enter the second number:"));
+//callback function in js:*A callback function is a function that is passed as an argument to another function
+//                        *It is commonly used for: handling asynchronous operations(like API calls,timeout,or event)
+//                        *Customizing the behaviour inside function 
+function First(){
+    console.log("This is muy first functoion");
+}
+First()
+function Second(callback){
+    console.log("This is my second function");
+    callback();
+}
+Second(First)
 
-
-
-alert(a+b);
+//using setTimeout 
+function First(){
+    console.log("This is muy first functoion");
+}
+First()
+function Second(callback){
+    console.log("This is my second function");
+    setTimeout(callback,2000);
+}
+Second(First) setTimeout
